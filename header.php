3451bec742cf7aaefb899e07a9a27f05eaf8eb06
patrick <patrickplumridge@gmail.com>
2015-11-29 15:28:09 -0400

    header

--- header.php
+++ header.php
@@ -9,12 +9,11 @@
 <ul>
      <li><a href="home.php">Home</a></li>
      <li><a href="profile.php">Profile</a></li>
-     <li><a href="login.php">Login</a></li>
-     <li><a href="register.php">Register</a></li>
-     <li><a href="settings.php">Settings</a></li>
      <li><a href="upload.php">Upload</a></li>
-     <li><a href="demo_session1.php">T_Login</a></li>
-     <li><a href="demo_session2.php">T_Logout</a></li>
+     <li><a href="settings.php">Settings</a></li>
+     <li><a href="register.php">Register</a></li>
+     <li><a href="login.php">Login</a></li>
+     <li><a href="logout.php">Logout</a></li>
 </ul>
 
 

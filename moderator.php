422d484f0b19f0ea23d8732f3b2c1c890258ae41
Xiaoqi Zhao <henry.zhaoxiaoqi@gmail.com>
2015-11-29 17:34:04 -0400

    need to add moderator's right

--- moderator.php
+++ moderator.php
@@ -1,3 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
 <?php
     include('header.php');
-    ?>
+    include('lib/config.php');
+    if(!isset($_SESSION["moderator"]))
+    {
+    	header('Location:login.php');
+    	exit();
+    }
+    if($_SESSION["moderator"] == False)
+    {
+    	header('Location:home.php');
+    	exit("you are not moderator");
+    }
+?>
+<table>
+<tr>
+    <th width="400">reason</th>
+    <th width="80" align="middle">report</th>
+    <th width="80" align="middle">photo id</th>
+    <th>photo</th>
+</tr>   
+<tr>
+<?php
+    $dbh = useDatabase();
+
+    $sql = $dbh->prepare('SELECT * FROM report');
+    $sql->execute();
+    foreach ($sql->fetchAll() as $row) {
+        $img_id = $row["pnum"];
+        print("<tr><td>" . $row['reason'] . '</td><td align="middle">' . $row['unum'] . '</td><td align="middle">' . $img_id .  "</td><td>" . "<img src='img/$img_id.jpg' align='middle' height=150>" . "</td></tr>");
+    }
+    
+?>
+</body>
+</html>
